e3c323b18ae6a47b9482569476b2cc2f022ca902
Benjamín Sánchez <bjsanchezb@gmail.com>
2024-11-18 19:47:49 +0100

    refactor: return figures
    make all plot functions return the figure, for easier collection later. Jupyter lab needs not to store it, as it plots it by default

--- keyforge/plots.py
+++ keyforge/plots.py
@@ -8,13 +8,14 @@ from keyforge.dataframes import SETS, HOUSES, set_house_df
 
 def plot_deck_overview(deck_df):
     """Plot number of decks per house/stat as heatmap"""
-    plt.figure(figsize = (12,3))
+    f = plt.figure(figsize = (12,3))
     _ = sns.heatmap(set_house_df(deck_df), cmap="RdYlGn", annot=True, cbar=False)
+    return f
 
 
 def plot_time_changes(match_df):
     """Plot changes in decks/matches over time"""
-    _ , ax = plt.subplots(1, 4, figsize=(24,6))
+    f , ax = plt.subplots(1, 4, figsize=(24,6))
 
     # Number of decks over time:
     ax[0].plot(pd.to_datetime(match_df["date"]), match_df["number_decks"])
@@ -33,26 +34,29 @@ def plot_time_changes(match_df):
     # Cumulative percentage completion over time:
     ax[3].plot(pd.to_datetime(match_df["date"]), match_df["perc_completion"])
     ax[3].set_ylabel("Cumulative percentage completion")
-    plt.show()
+
+    return f
 
 
 def plot_match_results(match_hm):
     """Plot results of matches as heatmap"""
-    plt.figure(figsize = (16,16))
+    f = plt.figure(figsize = (16,16))
     _ = sns.heatmap(match_hm, cmap="RdYlGn", square=True, annot=True, cbar=False)
+    return f
 
 
 def plot_deck_stats(deck_df):
     """Plot deck stats as heatmap"""
-    plt.figure(figsize = (10,10))
+    f = plt.figure(figsize = (10,10))
     data = deck_df.iloc[:,[7, 11, 12, 13]]
     labels = [f"{idx}: {row[0]} ({row[1]} - {row[2]} / {row[3]} / {row[4]})" for idx, row in deck_df.iterrows()]
     _ = sns.heatmap(data, yticklabels=labels, cmap="RdYlGn", annot=True, fmt="g", cbar=False)
+    return f
 
 
 def plot_win_vs_sas(deck_df):
     """Plot for each deck win% vs SAS (color by set) + trendline"""
-    _ , ax = plt.subplots(1, figsize=(10,6))
+    f , ax = plt.subplots(1, figsize=(10,6))
 
     # Plot data:
     for deck_set in SETS:
@@ -69,22 +73,25 @@ def plot_win_vs_sas(deck_df):
     ax.plot(x, p(x), color="grey", linewidth=0.5)
 
     win_sas_plot_settings(ax)
-    plt.show()
+
+    return f
 
 
 def plot_group_heatmaps(group_df, group_plays_hm, group_wins_hm, fig_length):
     """Group heatmaps: stats - match plays - match win rates"""
-    _ , (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(fig_length,6))
+    f , (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(fig_length,6))
 
     filtered_df = group_df.iloc[:,[0, 1, 5, 6, 7, 8]]
     _ = sns.heatmap(filtered_df, cmap="RdYlGn", ax=ax1, square=True, annot=True, fmt="g", cbar=False)
     _ = sns.heatmap(group_plays_hm, cmap="RdYlGn", ax=ax2, square=True, annot=True, fmt="g", cbar=False)
     _ = sns.heatmap(group_wins_hm, cmap="RdYlGn", ax=ax3, square=True, annot=True, fmt="g", cbar=False)
 
+    return f
+
 
 def plot_set_trends(set_df):
     """Set plots: Average win rates vs SAS - Changes in SAS overtime - Changes in win rate overtime"""
-    _ , ax = plt.subplots(1, 3, figsize=(20,6))
+    f , ax = plt.subplots(1, 3, figsize=(20,6))
 
     # Average win rates vs SAS (color by set):
     for deck_set in SETS:
@@ -105,10 +112,12 @@ def plot_set_trends(set_df):
     ax[2].set_ylabel("Win rate [%]")
     _ = ax[2].set_ylim(ymin=20, ymax=80)
 
+    return f
+
 
 def plot_house_trends(house_df):
     """House plot: Average win rates vs SAS"""
-    _ , ax = plt.subplots(1, 1, figsize=(15,5))
+    f , ax = plt.subplots(1, 1, figsize=(15,5))
 
     for deck_house in HOUSES:
         x = house_df.loc[deck_house, "avg_deck_sas"]
@@ -118,7 +127,8 @@ def plot_house_trends(house_df):
         ax.errorbar(x, y, yerr=yerr, fmt="-o", color=HOUSES[deck_house])
 
     win_sas_plot_settings(ax, xlim=[60, 90])
-    plt.show()
+
+    return f
 
 
 def win_sas_plot_settings(ax, xlim=None):
@@ -129,3 +139,4 @@ def win_sas_plot_settings(ax, xlim=None):
         ax.set_xlim(xmin=xlim[0], xmax=xlim[1])
     ax.set_ylim(ymin=0, ymax=100)
     ax.legend()
+    
